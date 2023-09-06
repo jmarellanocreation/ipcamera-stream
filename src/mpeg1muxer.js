3591f3d875693cbdaf546e44466eed1a44d66ed9
John Arellano <you@example.com>
2023-09-06 17:01:11 +0800

    Fix relaunch after conversion failed

--- src/mpeg1muxer.js
+++ src/mpeg1muxer.js
@@ -2,16 +2,55 @@ const child_process = require('child_process');
 const EventEmitter = require('events');
 
 class Mpeg1Muxer extends EventEmitter {
-
   #stream;
+  #configs;
 
   constructor(configs) {
-    super(configs);
-    this.#stream = child_process.spawn('ffmpeg', ['-rtsp_transport', 'tcp', '-i', configs.rtspUrl, '-f', 'mpegts', '-codec:v', 'mpeg1video', '-s', configs.screen, '-b:v', configs.bitrateVideo, '-r', configs.fps, '-muxdelay', '0.4', `http://localhost:${configs.rtspPort}/${configs.secret}`], {
+    super();
+    this.#configs = configs;
+    this.startFFmpeg();
+  }
+
+  startFFmpeg() {
+    this.#stream = child_process.spawn('ffmpeg', [
+      '-rtsp_transport', 'tcp',
+      '-i', this.#configs.rtspUrl,
+      '-f', 'mpegts',
+      '-codec:v', 'mpeg1video',
+      '-s', this.#configs.screen,
+      '-b:v', this.#configs.bitrateVideo,
+      '-r', this.#configs.fps,
+      '-muxdelay', '0.4',
+      `http://localhost:${this.#configs.rtspPort}/${this.#configs.secret}`
+    ], {
       detached: false
     });
-    this.#stream.stdout.on('data', (data) => { console.log(data) })
-    this.#stream.stderr.on('data', (data) => { console.error(data.toString()) })
+
+    this.#stream.on('error', (err) => {
+      console.error('FFmpeg Error:', err);
+    });
+
+    this.#stream.stdout.on('data', (data) => {
+      console.log('FFmpeg Data:', data);
+    });
+
+    this.#stream.stderr.on('data', (data) => {
+      if (data.toString().includes("Conversion failed!")) {
+        console.log('Restarting FFmpeg...');
+        this.startFFmpeg();
+      }
+      console.log('FFmpeg Log Data:', data);
+    });
+
+    this.#stream.on('exit', (code) => {
+      if (code === 0) {
+        console.log('FFmpeg process exited successfully.');
+      } else {
+        console.error('FFmpeg process exited with code:', code);
+        console.log('Restarting FFmpeg...');
+        this.startFFmpeg();
+      }
+    });
   }
 }
 
