9be6acf3fca42619e216283bed003ed8c05c2e10
John Arellano <you@example.com>
2023-09-11 16:26:33 +0800

    Update mpeg1muxer to kill previous process

--- src/mpeg1muxer.js
+++ src/mpeg1muxer.js
@@ -4,6 +4,7 @@ const EventEmitter = require('events');
 class Mpeg1Muxer extends EventEmitter {
   #stream;
   #configs;
+  #restartTimer; // Timer reference for restart delay
 
   constructor(configs) {
     super();
@@ -12,6 +13,11 @@ class Mpeg1Muxer extends EventEmitter {
   }
 
   startFFmpeg() {
+    if (this.#stream) {
+      this.#stream.kill('SIGTERM'); // Stop the existing FFmpeg process if it exists
+      this.#stream = null; // Clear the reference to the process
+    }
+
     this.#stream = child_process.spawn('ffmpeg', [
       '-rtsp_transport', 'tcp',
       '-i', this.#configs.rtspUrl,
@@ -28,6 +34,8 @@ class Mpeg1Muxer extends EventEmitter {
 
     this.#stream.on('error', (err) => {
       console.error('FFmpeg Error:', err);
+      this.stopFFmpeg();
+      this.scheduleRestart(); // Schedule restart after a delay
     });
 
     this.#stream.stdout.on('data', (data) => {
@@ -37,7 +45,8 @@ class Mpeg1Muxer extends EventEmitter {
     this.#stream.stderr.on('data', (data) => {
       if (data.toString().includes("Conversion failed!")) {
         console.log('Restarting FFmpeg...');
-        this.startFFmpeg();
+        this.stopFFmpeg();
+        this.scheduleRestart(); // Schedule restart after a delay
       }
       console.log('FFmpeg Log Data:', data);
     });
@@ -48,9 +57,28 @@ class Mpeg1Muxer extends EventEmitter {
       } else {
         console.error('FFmpeg process exited with code:', code);
         console.log('Restarting FFmpeg...');
-        this.startFFmpeg();
+        this.scheduleRestart(); // Schedule restart after a delay
       }
     });
+  }
+
+  // Method to stop the FFmpeg process
+  stopFFmpeg() {
+    if (this.#stream) {
+      this.#stream.kill('SIGTERM');
+      this.#stream = null;
+    }
+  }
+
+  // Method to schedule restart after a 3-second delay
+  scheduleRestart() {
+    if (this.#restartTimer) {
+      clearTimeout(this.#restartTimer); // Clear previous timer if it exists
+    }
+
+    this.#restartTimer = setTimeout(() => {
+      this.startFFmpeg(); // Restart FFmpeg after the timeout
+    }, 3000); // 3 seconds delay (3000 milliseconds)
   }
 }
 
